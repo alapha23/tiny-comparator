30c0b3125b68bcdde3b2e1b9329f61a2f132af5a
root <root@localhost.localdomain>
2017-12-22 14:10:22 +0900

    Initial commit

---
+++ gcc/tiny-comparator.h
@@ -0,0 +1,13 @@
+#include "tree.h"
+
+
+#define PREV_STMT(NODE) TREE_TYPE (NODE)
+#define NEXT_STMT(NODE) TREE_CHAIN (NODE)
+/*
+extern void double_chain_stmts			PARAMS ((tree))
+extern void double_chain_free			PARAMS ((tree))
+extern void dchain_insert_stmt_after		PARAMS ((tree))
+extern void dchain_insert_stmt_before		PARAMS ((tree))
+*/
+
+
