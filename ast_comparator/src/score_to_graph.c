8ac632e551cb794a365969576c4c3d4cdfbe6a8e
alapha23 <alapha23@gmail.com>
2018-02-09 02:55:02 +0900

    add threshold

--- ast_comparator/src/score_to_graph.c
+++ ast_comparator/src/score_to_graph.c
@@ -16,6 +16,8 @@ int main(int argc, char **argv)
 	
 	fill_pool();
 
+	cur_all_dot("eval");
+
 	fclose(fp);
 	free(pool);
 	return 0;
@@ -53,9 +55,165 @@ void emit_header(char *scope, char *whovswho)
 void fill_pool(void)
 {
 	char line[128];
+	char scope[64];
 
-	readline(line, 128, fp);
+	while(1)
+	{
+		if(NULL == readline(line, 128, fp))
+		{
+			// EOF
+			break;
+		}
+		if(is_scope(line))	
+		{
+			sscanf("SCOPE: %s", scope);
+		}
+		else
+		{
+			// Add a new node
+			if(add_to_node(line) == NULL)
+				break;	
+			//dump_node(n);
+		}
+	}
+}
 
-	DEBUF("%s", line);
+node *add_to_node(char *line)
+{
+	// has myname existed in pool?
+	char *myname = calloc(64, sizeof(char));
+	node *n;
+	plagiarism *p;
 
+	assert(NULL != line);
+	if(*line == ' ')
+		return NULL;
+
+	sscanf(line, "| %s ", myname);
+	
+	n = search_pool(myname);
+	if(n == NULL)
+	{
+		// not exist, build a new node
+		n = (node *)calloc(1, sizeof(node));
+		// add new node to pool
+		*(pool+n_inpool) = n;
+		n->id = n_inpool;
+		strcpy(n->filename, myname);
+		n_inpool++;
+	}
+
+	// ignore plagiarism lower than 40%
+	float match;
+	sscanf(line, "| %*s | %*s | %f | %*s ", &match);
+	match = match *100;
+//	DEBUF("match: %.2f", match);
+//	DEBUF("Threshold %d", Tolerance);
+	if(match > (float)Tolerance)
+	{
+		// build a new plagiarism relationship
+		n->n_plagiarism++;
+
+		p = calloc(1, sizeof(plagiarism));
+		sscanf(line, "| %*s | %s | %f | %s ", p->hername, &p->match, p->scope);
+		p->target_id = n->n_plagiarism;
+	       	p->hertype = check_type(p->hername);
+
+		// add plagiarsm
+		assert(n->n_plagiarism != N_REF);
+		n->plag[n->n_plagiarism-1] = p;
+
+		free(myname);
+		return n;
+	}
+	else
+		return n;
+}
+
+int check_type(char *hername)
+{
+	if(*(hername) == 'r')
+	{
+	// reference
+		return 2;
+	}else if (*(hername) == 'p')
+	{
+		return 1;
+	}
+	else
+		return 0;
+}
+
+node *search_pool(char *myname)
+{
+	node *ret = NULL;
+	int i = n_inpool;
+	while(i)
+	{
+		ret = *(pool + i - 1);
+//		printf("myname:%s vs pool:%s\n", myname, ret->filename);
+//		fflush(stdout);
+		if(!strcmp(myname, ret->filename))
+			return ret;
+		i--;
+	}
+	return NULL;
+}
+
+void dump_node(node *n)
+{
+	int i = n->n_plagiarism;
+	plagiarism *p;
+	DEBUF("node id: %d", n->id);
+	DEBUF("my name: %s", n->filename);
+	DEBUF("Num of plagiarisms: %d", n->n_plagiarism);
+	while(i)
+	{
+		p = n->plag[i-1];
+		DEBUF("Plagiarism id: %d", p->target_id);
+		DEBUF("hername : %s", p->hername);
+		DEBUF("Hertype: %d", p->hertype);
+		DEBUF("scope: %s", p->scope);
+		DEBUF("match: %f", p->match);
+		i--;
+	}
+}
+
+void cur_all_dot(char *scope)
+{
+	int iter = n_inpool-1;
+	node *n;
+	plagiarism *p;
+	char *id1 = calloc(32, sizeof(char));
+	char *id2 = calloc(32, sizeof(char));
+	char link_label[64];
+	emit_header(scope, "cur vs. prev");
+	while(iter)
+	{
+		n = *(pool+iter-1);
+		int i = n->n_plagiarism;
+		sscanf(n->filename, "%[^.]%*s", id1);
+		dot_box(id1, n->filename);
+		while(i)
+		{
+			p = n->plag[i-1];
+			// check if scope meets condition
+			if(strcmp(scope,p->scope))
+			{
+//				fprintf(stdout,"Not this scope: %s compared to %s\n", (n->plag)[i-1]->scope, scope);
+
+				continue;
+			}
+			// plagiarism dots
+			sscanf((n->plag)[i-1]->hername, "%[^.]", id2);
+			dot_ellipse(id2, (n->plag)[i-1]->hername);
+			sprintf(link_label, "%s: %.1f\%", scope, (n->plag)[i-1]->match*100.0);
+			dot_link(id1, id2, link_label);
+			i--;
+		}
+		iter--;
+	}
+	emit_footer();
+	free(id1);
+	free(id2);
 }
--- ast_comparator/src/score_to_graph.h
+++ ast_comparator/src/score_to_graph.h
@@ -6,21 +6,38 @@
 #include <string.h>
 #include <assert.h>
 
-#define N_REF	512	// Number of references to be compared to
+#define N_REF	2<<14	// Number of references to be compared to
 #define	N_SCOPE	32
 #define N_CUR	128
+#define Tolerance	30	// threshold. Plagiarism Below are ignored
 
 #define DEBUG(a) fprintf(stderr, #a"\n"); \
 	fflush(stderr)
 #define DEBUF(a, b)	fprintf(stderr, a"\n", (b)); \
 	fflush(stderr)
-#define readline(line, size, fp) if(fgets((line), 128, fp)==NULL) \
-	{DEBUG(Unexpected reached EOF); exit(0);}
+#define readline(line, size, fp) \
+       	({ char *_flag; _flag = line;\
+	 if(fgets((line), 128, fp)==NULL) \
+		{	\
+			DEBUG(reached EOF); \
+			_flag = NULL;	\
+		}\
+		_flag;\
+	})
+#define is_scope(line) (*(line) == 'S'? (1):(0))
+
+#define emit_footer()	({fprintf(stdout, "\n}\n"); fflush(stdout);})
+
+#define dot_box(_id, label)	({fprintf(stdout, "  node%s [label=\"%s\", shape=box];\n", _id, label); fflush(stdout);})
+
+#define dot_ellipse(id,label)	({fprintf(stdout, "  node%s [label=\"%s\", shape=ellipse];\n", id, label); fflush(stdout);\
+		})
+#define dot_link(id1, id2, label)	({fprintf(stdout, "  node%s -> node%s [label=\"%s\"]", id1, id2, label);}) 
+
 
 typedef struct _p
 {
-	char *myname;
-	char *hername;
+	char hername[64];
 	char scope[64];	// the relationship exist within a scope
 	int target_id;	// my target_id of my parent node
 	int hertype;	// 0 for cur, 1 for prev, 2 for ref
@@ -29,10 +46,10 @@ typedef struct _p
 
 typedef struct _n
 {
-	int id;			// id in pool
+	int id;			// id in pool starting from 0. *(pool+id)
 	char filename[64];
-	int n_target;		// how many target node has
-	plagiarism target[N_REF];
+	int n_plagiarism;		// how many target node has
+	plagiarism *plag[N_REF];
 } node;				// node stands of the left hand side of comparison
 
 // fileptr for Similarity_tsh.txt
@@ -52,15 +69,31 @@ size_t get_filesize(void);
 // put all data inside the pool
 void fill_pool(void);
 
+// add a node
+node *add_to_node(char*);
+
+// search node in pool
+node *search_pool(char *);
+
+// decide hertype from her name
+int check_type(char *);
 
 // emit_header
 void emit_header(char *scope, char *whovswho);
 
 // Relationship between current & ref > cur_ref_tsh.dot
+void cur_ref_dot(char *);
+
 // Relationship between current & prev
+void cur_prev_dot(char *);
+
 // Relationship between current & cur
+void cur_cur_dot(char *);
+
 // Relationship between current & ALL
+void cur_all_dot(char *);
 
-
+// only for debugging
+void dump_node(node *);
 
 #endif
